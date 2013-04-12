f10685db5e14afedd43d048ff756de1e5c8686fd
haosdent <haosdent@gmail.com>
2013-04-12 13:45:57 +0800

    Create the repo.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject hci-dispenser "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler hci-dispenser.handler/app}
+  :profiles
+  {:dev {:dependencies [
+                        [ring-mock "0.1.3"]
+                        [ring/ring-json "0.2.0"]
+                        ]}})
