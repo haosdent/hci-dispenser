0c1744baa788f38840726aea2bc810c49fa7f921
haosdent <haosdent@gmail.com>
2013-04-15 02:53:12 +0800

    Update description in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject hci-dispenser "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The server side of hci-dispenser."
+  :url "https://github.com/haosdent/hci-dispenser"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
