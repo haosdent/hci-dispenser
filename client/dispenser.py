ea1ae7c47384f7e9dc87c33e1c5f5cfd4b080aaa
haosdent <haosdent@gmail.com>
2013-04-16 02:28:51 +0800

    Finish client and combine code.

--- client/dispenser.py
+++ client/dispenser.py
@@ -1,15 +1,18 @@
 import RPIO
-import urllib2, urllib
+import urllib2
 
 def status_change_trigger(id, val):
+    val = (val + 1) % 2
     print 'Status change: gpio_id:', id, ', value:', val
 
-    data = urllib.urlencode({'status': val})
-    url = 'http://hci-dispenser.cloudfoundry.com/status'
-    result = urllib2.urlopen(url, data).read()
+    data = '{"status": ' + str(val) + '}'
+    url = 'http://dispenser.cloudfoundry.com/status'
+    req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+    result = urllib2.urlopen(req).read()
     print 'Post result:', result
 
     return result
 
-gpio_id = 17
+gpio_id = 4
 RPIO.add_interrupt_callback(gpio_id, status_change_trigger)
+RPIO.wait_for_interrupts()
--- server/project.clj
+++ server/project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [cheshire "5.1.1"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler org.scauhci.dispenser.routes/app}
   :profiles
   {:dev {:dependencies [
                         [ring-mock "0.1.3"]
-                        [cheshire "5.1.1"]
                         ]}})
